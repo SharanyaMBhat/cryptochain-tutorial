af76f21274708d9ea50604da250e5e651983e4af
74951523 <sharanya.m@target.com>
2020-05-14 21:12:32 +0530

    Section 6 - 55

--- wallet/index.js
+++ wallet/index.js
@@ -1,5 +1,6 @@
 const { STARTING_BALANCE} = require('../config');
 const {ec, cryptoHash} = require('../util'); 
+const Transaction = require('./transaction')
 class Wallet{
     constructor(){
         this.balance = STARTING_BALANCE;
@@ -11,6 +12,12 @@ class Wallet{
 return this.keyPair.sign(cryptoHash(data));
     }
      
+createTransaction({recipient,amount}){
+    if(amount>this.balance){
+        throw new Error('Amount exceeds balance')
+    }
+    return new Transaction({senderWallet:this,recipient,amount});
+}
 }
 
 module.exports = Wallet;
--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,5 +1,6 @@
 const Wallet = require('./index');
 const {verifySignature} = require('../util')
+const Transaction = require('./transaction')
 describe('Wallet', () => {
     let wallet;
 
@@ -38,5 +39,37 @@ expect(
         });
    });
 
+   describe('createTransaction()',()=>{
+       describe('and amount exceeds the balance',()=>{
+        it('throws an error',()=>{
+            expect(()=>{
+                wallet.createTransaction({amount: 999999, recipient: 'foo-recipient'})
+                .toThrow('Amount exceeds balance');
+            })
+        })
+       })
+
+       describe('and amount is valid',()=>{
+           let transaction,amount,recipient;
+
+        beforeEach(()=>{
+            amount = 50;
+            recipient = 'foo-recipient';
+            transaction = wallet.createTransaction({amount,recipient});
+        })
+        it('creates instance of `Transaction`',()=>{
+            expect(transaction instanceof Transaction).toBe(true);
+        })
+
+        it('matches transaction input with the wallet',()=>{
+            expect(transaction.input.address).toEqual(wallet.publicKey)
+        })
+
+        it('outputs the amount the recipient',()=>{
+            expect(transaction.outputMap[recipient]).toEqual(amount)
+        })
+    })
+
+   })
   
 });
