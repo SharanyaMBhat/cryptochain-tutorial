cf9ff231f3a4733ecc82566815dcfb7ef0b5ab06
Sharanya M <sharanya.m@target.com>
2020-07-10 00:08:47 +0530

    Section 8 - 83

--- blockchain/index.test.js
+++ blockchain/index.test.js
@@ -1,7 +1,8 @@
 const Blockchain = require('../blockchain');
 const Block = require('./block');
 const {cryptoHash} = require('../util'); 
- 
+const Wallet = require('../wallet')
+const Transaction = require('../wallet/transaction')
 describe('Blockchain',() => {
 
     let blockchain, newChain, originalChain;
@@ -153,5 +154,88 @@ describe('Blockchain',() => {
            
         });
     })
+
+    describe('validTransactionData()', () => {
+        let transaction, rewardTransaction, wallet
+
+        beforeEach(()=>{
+            wallet = new Wallet();
+            transaction = wallet.createTransaction({
+                recipient: 'foo',
+                amount: 65
+            })
+            rewardTransaction = Transaction.rewardTransaction({
+                minerWallet: wallet
+            })
+        })
+
+        describe('and the transaction data is valid', () => {
+            it('returns true',()=>{
+                newChain.addBlock({
+                    data: [transaction,rewardTransaction]
+                })
+
+                expect(
+                blockchain.validTransactionData({
+                    chain:newChain.chain
+                })).toBe(true)
+
+            })
+        })
+
+        describe('and the transaction data has multiple rewards', () => {
+            it('returns false',()=>{
+
+                newChain.addBlock({
+                    data:[transaction,rewardTransaction,rewardTransaction]
+                })
+
+                expect(
+                    blockchain.validTransactionData({
+                        chain:newChain.chain
+                    })).toBe(false)
+            })
+        })
+
+        describe('and the transaction data has atleast one malformed outputMap', () => {
+           
+            describe('and the transaction is not a reward transaction', () => {
+                it('returns false',()=>{
+                    transaction.outputMap[wallet.publicKey] = 999999;
+
+                    newChain.addBlock({
+                        data:[transaction,rewardTransaction]
+                    })
+    
+                    expect(
+                        blockchain.validTransactionData({
+                            chain:newChain.chain
+                        })).toBe(false)
+                })
+            })
+           
+           
+            describe('and the transaction is a reward transaction', () => {
+                it('returns false',()=>{
+                    
+                })
+            })
+        })
+
+        describe('and the transaction data has atleast one malformed input', () => {
+            it('returns false',()=>{
+                
+            })
+        })
+
+        describe('and a block contains duplicate transactions', () => {
+            it('returns false',()=>{
+                
+            })
+        })
+        
+        
+    })
+    
 
   });
