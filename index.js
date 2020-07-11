bb667805fd67a988944b06897b6d6d8b410b9009
Sharanya M <sharanya.m@target.com>
2020-07-12 04:02:06 +0530

    Section 10 - 100

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const app  = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
+const walletFoo = new Wallet();
+const walletBar = new Wallet();
 const pubsub = new PubSub({blockchain, transactionPool});
 const transactionMiner = new TransactionMiner({blockchain,transactionPool,wallet,pubsub});
 const DEFAULT_PORT = 3000;
@@ -97,6 +99,45 @@ console.log('replace chain on a async with', rootChain);
         }
     })
 }
+
+const generateWalletTransaction = ({wallet, recipient, amount}) =>{
+    const transaction = wallet.createTransaction({
+        recipient,amount,chain:blockchain.chain
+    });
+
+    transactionPool.setTransaction(transaction);
+}
+
+
+const walletAction = ()=> generateWalletTransaction({
+    wallet,recipient:walletFoo.publicKey, amount:5
+})
+
+const walletFooAction = ()=> generateWalletTransaction({
+    wallet: walletFoo, recipient:walletBar.publicKey, amount:10
+})
+
+const walletBarAction = ()=> generateWalletTransaction({
+    wallet: walletBar,recipient:wallet.publicKey, amount:15
+})
+
+for(let i=0;i<10;i++){
+    if(i%3===0){
+        walletAction();
+        walletFooAction();
+    }
+    else if(i%3===1){
+        walletAction();
+        walletBarAction();
+    }  
+    else{ 
+        walletFooAction();
+        walletBarAction();
+    }  
+
+    transactionMiner.mineTransactions();
+}
+
 let PEER_PORT;
 
 if(process.env.GENERATE_PEER_PORT === 'true'){
